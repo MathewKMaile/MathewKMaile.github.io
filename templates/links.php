95088bcc6165e358c140a106b72a040cc53fa2f0
mathew maile <57395379+MathewKMaile@users.noreply.github.com>
2021-03-20 21:02:19 +0000

    Delete templates directory

--- templates/links.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="https://www.mathewmaile.com/assets/css/footer.css">
-<link rel="stylesheet" href="https://www.mathewmaile.com/assets/css/navbar.css">
-<link rel="stylesheet" href="https://www.mathewmaile.com/assets/css/photo.css">
-<link href="https://fonts.googleapis.com/css?family=Arvo&display=swap" rel="stylesheet">
